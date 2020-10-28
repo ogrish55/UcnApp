344e9b6359c70ef0d2f50241f4bacf9a6559062f
Jonas Haxholm <jonashaxholm@hotmail.dk>
2020-10-28 09:17:18 +0100

    Små opgaver som fjern første entry i array

--- vueapp/src/components/Dashboard.vue
+++ vueapp/src/components/Dashboard.vue
@@ -61,8 +61,8 @@
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Forbrug i m3</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ usageInM3 }} m3</div>
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Forbrug i m<sup>3</sup></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ usageInM3 }} M<sup>3</sup></div>
                       </div>
                       <div class="col-auto">
                         <i class="fas fa-home fa-2x text-gray-300"></i>
@@ -182,8 +182,8 @@
                   <!-- Card Body -->
                   <div class="card-body">
                     <div class="chart-area">
-                      <line-chart :width="300" :height="70" v-if="ready" :chart-data="datacollection"
-                                  :options="chartOptions"></line-chart>
+                      <bar-chart :width="300" :height="70" v-if="ready" :chart-data="datacollection"
+                                  :options="chartOptions"></bar-chart>
                     </div>
                   </div>
                 </div>
@@ -413,12 +413,14 @@ import '../assets/js/sb-admin-2.min.js'
 import '../assets/vendor/chart.js/Chart.min.js'
 import '../assets/vendor/chart.js/Chart.min.js'
 import LineChart from './LineChart'
+import BarChart from './BarChart'
 import axios from 'axios'
 // import '../assets/js/demo/chart-pie-demo.js';
 export default {
   name: 'Dashboard',
   components: {
-    LineChart
+    LineChart,
+    BarChart
     },
   data () {
     return {
@@ -432,7 +434,28 @@ export default {
       coldTimeOfReader: [],
       reader: null,
       coldReader: null,
-      chartOptions: null,
+      chartOptions: {
+        scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: false
+              }
+            }]
+          },
+          legend: {
+            display: true
+          },
+          responsive: true,
+          maintainAspectRatio: true
+      },
       usageInDkk: null,
       usageInM3: null,
       aconto: null,
@@ -479,7 +502,11 @@ export default {
           {
             label: 'Varmt vand',
             backgroundColor: '#f87979',
-            data: this.measurements
+            data: this.measurements,
+            fill: false,
+            borderWidth: 1,
+            borderColor: '#f87979',
+            backgroundColor: '#f87979'
           }
         ]
       }
@@ -492,7 +519,11 @@ export default {
           {
             label: 'Koldt vand',
             backgroundColor: '#0a5493',
-            data: this.coldMeasurements
+            data: this.coldMeasurements,
+            fill: false,
+            borderWidth: 1,
+            borderColor: '#0a5493',
+            backgroundColor: '#0a5493'
           }
         ]
       }
@@ -510,7 +541,8 @@ export default {
 
         this.timeOfReader.push(this.monthNames[dateTime.getMonth()] + " '" + year)
       })
-      // this.timeOfReader = this.timeOfReader.map(x => x.substr(0, 10))
+      this.measurements.shift(); // fjerner første element af array da data altid vil være 0
+      this.timeOfReader.shift(); // fjerner tilsvarende label
     },
     getColdDataFromReader () {
       let keys = Object.keys(this.coldReader)
@@ -522,9 +554,9 @@ export default {
         year = year.toString().slice(-2) // "19"
 
         this.coldTimeOfReader.push(this.monthNames[dateTime.getMonth()] + " '" + year)
-        // this.coldTimeOfReader.push(this.monthNames[dateTime.getMonth()])
       })
-      // this.coldTimeOfReader = this.coldTimeOfReader.map(x => x.substr(0, 10))
+      this.coldMeasurements.shift(); // fjerner første element af array da data altid vil være 0
+      this.coldTimeOfReader.shift(); // fjerner tilsvarende label
     },
     apiCalls () {
       axios
