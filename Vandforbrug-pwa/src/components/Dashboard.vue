1918982c59169ad42466f73dc9db793dbcf63b44
Jonas Haxholm <jonashaxholm@hotmail.dk>
2020-11-12 11:07:32 +0100

    kold vand gennemsnitslinje + ændret lidt farver

--- Vandforbrug-pwa/src/components/Dashboard.vue
+++ Vandforbrug-pwa/src/components/Dashboard.vue
@@ -51,7 +51,6 @@
 
           <!-- Begin Page Content -->
           <div class="container-fluid">
-
             <!-- Content Row -->
             <div class="row" id="cardrow">
 
@@ -62,7 +61,7 @@
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Forbrug i m<sup>3</sup></div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ usageInM3 }} M<sup>3</sup></div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ usageInM3 }} M<sup>3</sup></div> 
                       </div>
                       <div class="col-auto">
                         <i class="fas fa-home fa-2x text-gray-300"></i>
@@ -74,11 +73,11 @@
 
               <!-- Forbrug i DKK -->
               <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
+                <div class="card border-left-danger shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Forbrug i DKK</div>
+                        <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Forbrug i DKK</div>
                         <div v-bind="calculateUsageInDkk" class="h5 mb-0 font-weight-bold text-gray-800">
                           {{ this.usageInDkk }} DKK
                         </div>
@@ -93,11 +92,11 @@
 
               <!-- Betalt aconto -->
               <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
+                <div class="card border-left-warning shadow h-100 py-2">
                   <div class="card-body">
                     <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Betalt aconto</div>
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Betalt aconto</div>
                         <div v-bind="calculateAconto" class="h5 mb-0 font-weight-bold text-gray-800">{{ aconto }} DKK
                         </div>
                       </div>
@@ -437,7 +436,7 @@ export default {
           },
           { // linje med gennemsnit
             label: 'Gennemsnit',
-            data: this.averageHotArr,
+            data: this.averageColdArr,
             type: 'line',
             fill: false,
             borderColor: '#40c940',
@@ -486,8 +485,10 @@ export default {
       this.coldTimeOfReader.shift(); // fjerner tilsvarende label
     },
     getMonthDataFromReader() {
+      // Clear out the arrays
       this.monthTimeOfReader = []
       this.monthMeasurements = []
+
       let keys = Object.keys(this.monthReader)
       keys.forEach(key => {
         this.monthMeasurements.push(this.monthReader[key].value)
