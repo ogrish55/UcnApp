bc297f5bdfb1851e791b03d6e16a6bb1967c89cb
ogrish55 <kristoffer92k@hotmail.com>
2020-11-18 14:22:36 +0100

    made footer stick to bottom of page

--- Vandforbrug-pwa/src/App.vue
+++ Vandforbrug-pwa/src/App.vue
@@ -52,5 +52,8 @@ export default {
   /* gradient baggrund */
   background: rgb(2, 0, 36);
   background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(73, 87, 255, 0.4150035014005602) 0%, rgba(255, 0, 0, 0.2553396358543417) 100%);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 </style>
