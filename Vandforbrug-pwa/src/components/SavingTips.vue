6ef5d255c043e04318b587dc7dabbc29381a0a54
akselsoeborg <akselsoeborg@gmail.com>
2020-11-20 11:11:37 +0100

    animeret liste, stadig ikke kønt :)

--- Vandforbrug-pwa/src/components/SavingTips.vue
+++ Vandforbrug-pwa/src/components/SavingTips.vue
@@ -3,17 +3,20 @@
         <h1>
             Hvordan sparer jeg på vandet?
         </h1>
+        <p>
+          Vi bruger meget vand i dagligdagen - når vi går i bad, børster tænder og går på toilettet eller når vi vasker tøj, vasker bil og vasker op. Se her, hvordan du nemt kan spare penge og samtidig skåne miljøet.
+        </p>
         <ul class="list-group">
-            <li class="list-group-item">Undgå at skifte vand alt for tit i jeres badebassin. Det koster nemlig omkring 700 kroner at fylde et badebassin på 10.000 liter.</li>
-            <li class="list-group-item">Pas på toiletter der løber. Det kan koste flere tusinde kroner om året.</li>
-            <li class="list-group-item">Tag brusebad i stedet for karbad.</li>
-            <li class="list-group-item">Tag korte brusebade og luk for bruseren, når du sæber dig ind.</li>
-            <li class="list-group-item">Luk for vandet, når du børster tænder.</li>
-            <li class="list-group-item">Vand kun haven, hvis det er helt nødvendigt. Brug vandkande i stedet for slange og saml regnvand.</li>
-            <li class="list-group-item">Tænd kun opvaskemaskinen og vaskemaskinen, når den er helt fyldt.</li>
-            <li class="list-group-item">Brug en balje, hvis du vasker op i hånden.</li>
-            <li class="list-group-item">Køl dit vand i køleskabet i stedet for at lade vandhanen løbe, indtil vandet bliver koldt.</li>
-            <li class="list-group-item">Køb vaskemaskiner og opvaskemaskiner med lavt vandforbrug.</li>
+            <li class="list-group-item" style="--animation-order: 1;">Undgå at skifte vand alt for tit i jeres badebassin. Det koster nemlig omkring 700 kroner at fylde et badebassin på 10.000 liter.</li>
+            <li class="list-group-item" style="--animation-order: 2;">Pas på toiletter der løber. Det kan koste flere tusinde kroner om året.</li>
+            <li class="list-group-item" style="--animation-order: 3;">Tag brusebad i stedet for karbad.</li>
+            <li class="list-group-item" style="--animation-order: 4;">Tag korte brusebade og luk for bruseren, når du sæber dig ind.</li>
+            <li class="list-group-item" style="--animation-order: 5;">Luk for vandet, når du børster tænder.</li>
+            <li class="list-group-item" style="--animation-order: 6;">Vand kun haven, hvis det er helt nødvendigt. Brug vandkande i stedet for slange og saml regnvand.</li>
+            <li class="list-group-item" style="--animation-order: 7;" >Tænd kun opvaskemaskinen og vaskemaskinen, når den er helt fyldt.</li>
+            <li class="list-group-item" style="--animation-order: 8;">Brug en balje, hvis du vasker op i hånden.</li>
+            <li class="list-group-item" style="--animation-order: 9;">Køl dit vand i køleskabet i stedet for at lade vandhanen løbe, indtil vandet bliver koldt.</li>
+            <li class="list-group-item" style="--animation-order: 10;">Køb vaskemaskiner og opvaskemaskiner med lavt vandforbrug.</li>
         </ul>
     </div>
 </template>
@@ -25,13 +28,54 @@ export default {
 </script>
 
 <style scoped>
-    ul{
+    /* ul{
   margin:120;
-  padding:20px;
+  padding:50px;
 } 
 ul li    {
   padding:0;
   width:500px;
   margin:10px 0;
+} */
+h1 {
+  margin: 20px auto;
+  padding: 10px;
+}
+p {
+   margin: 10px;
+ }
+
+ul {
+  padding: 0;
+  display: block;
+  margin: 24px auto;
+  width: 320px;
+}
+
+li {
+  display: block;
+  height: 80px;
+  width: 400px;
+  background-color: #2fa6dd57;
+  animation-name: animateIn;
+  animation-duration: 350ms;
+  animation-delay: calc(var(--animation-order) * 100ms);
+  animation-fill-mode: both;
+  animation-timing-function: ease-in-out;
+}
+
+li + li {
+  margin-top: 3px;
+}
+
+@keyframes animateIn {
+  0% {
+    opacity: 0;
+    transform: scale(0.6) translateY(-8px);
+  }
+  
+  100% {
+    opacity: 1;
+  }
 }
 </style>
